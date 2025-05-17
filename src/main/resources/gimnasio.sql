41ef95edb675645d496412835149c0c8e87ed262
MarcosLS05 <marcoscurro1234@gmail.com>
2025-05-17 13:55:27 +0200

    avance ok

--- src/main/resources/gimnasio.sql
+++ src/main/resources/gimnasio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: mysql-ramboot
--- Tiempo de generación: 16-05-2025 a las 10:12:34
+-- Tiempo de generación: 17-05-2025 a las 11:16:17
 -- Versión del servidor: 9.2.0
 -- Versión de PHP: 8.2.27
 
@@ -32,24 +32,27 @@ CREATE TABLE `grupocontrata` (
   `titulo` varchar(255) NOT NULL,
   `descripcion` text NOT NULL,
   `id_usuario` bigint DEFAULT NULL,
-  `id_planesentrenamiento` bigint DEFAULT NULL
+  `id_planesentrenamiento` bigint DEFAULT NULL,
+  `creado_en` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `grupocontrata`
 --
 
-INSERT INTO `grupocontrata` (`id`, `titulo`, `descripcion`, `id_usuario`, `id_planesentrenamiento`) VALUES
-(1, 'Título aleatorio 0', 'Descripción aleatoria 0', 19, 5),
-(2, 'Título aleatorio 1', 'Descripción aleatoria 1', 13, 7),
-(3, 'Título aleatorio 2', 'Descripción aleatoria 2', 20, 5),
-(4, 'Título aleatorio 3', 'Descripción aleatoria 3', 2, 10),
-(5, 'Título aleatorio 4', 'Descripción aleatoria 4', 13, 10),
-(6, 'Título aleatorio 5', 'Descripción aleatoria 5', 17, 5),
-(7, 'Título aleatorio 6', 'Descripción aleatoria 6', 13, 5),
-(8, 'Título aleatorio 7', 'Descripción aleatoria 7', 19, 10),
-(9, 'Título aleatorio 8', 'Descripción aleatoria 8', 18, 3),
-(10, 'Título aleatorio 9', 'Descripción aleatoria 9', 10, 3);
+INSERT INTO `grupocontrata` (`id`, `titulo`, `descripcion`, `id_usuario`, `id_planesentrenamiento`, `creado_en`) VALUES
+(1, 'Título aleatorio 0', 'Descripción aleatoria 0', 19, 5, NULL),
+(2, 'Título aleatorio 1', 'Descripción aleatoria 1', 13, 7, NULL),
+(3, 'Título aleatorio 2', 'Descripción aleatoria 2', 20, 5, NULL),
+(4, 'Título aleatorio 3', 'Descripción aleatoria 3', 2, 10, NULL),
+(5, 'Título aleatorio 4', 'Descripción aleatoria 4', 13, 10, NULL),
+(6, 'Título aleatorio 5', 'Descripción aleatoria 5', 17, 5, NULL),
+(7, 'Título aleatorio 6', 'Descripción aleatoria 6', 13, 5, NULL),
+(8, 'Título aleatorio 7', 'Descripción aleatoria 7', 19, 10, NULL),
+(9, 'Título aleatorio 8', 'Descripción aleatoria 8', 18, 3, NULL),
+(10, 'Título aleatorio 9', 'Descripción aleatoria 9', 10, 3, NULL),
+(11, 'gggg', 'gggg', 4, 3, NULL),
+(12, 'prueba', 'de contrato', 5, 3, '2025-05-16');
 
 -- --------------------------------------------------------
 
@@ -74,25 +77,28 @@ CREATE TABLE `mensaje` (
 
 CREATE TABLE `planesentrenamiento` (
   `id` bigint NOT NULL,
+  `dificultad` varchar(255) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
   `titulo` varchar(255) NOT NULL,
-  `descripcion` text
+  `descripcion` text,
+  `fecha_creacion` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `planesentrenamiento`
 --
 
-INSERT INTO `planesentrenamiento` (`id`, `titulo`, `descripcion`) VALUES
-(1, 'Plan de entrenamiento 0', 'Descripción del plan de entrenamiento 0'),
-(2, 'Plan de entrenamiento 1', 'Descripción del plan de entrenamiento 1'),
-(3, 'Plan de entrenamiento 2', 'Descripción del plan de entrenamiento 2'),
-(4, 'Plan de entrenamiento 3', 'Descripción del plan de entrenamiento 3'),
-(5, 'Plan de entrenamiento 4', 'Descripción del plan de entrenamiento 4'),
-(6, 'Plan de entrenamiento 5', 'Descripción del plan de entrenamiento 5'),
-(7, 'Plan de entrenamiento 6', 'Descripción del plan de entrenamiento 6'),
-(8, 'Plan de entrenamiento 7', 'Descripción del plan de entrenamiento 7'),
-(9, 'Plan de entrenamiento 8', 'Descripción del plan de entrenamiento 8'),
-(10, 'Plan de entrenamiento 9', 'Descripción del plan de entrenamiento 9');
+INSERT INTO `planesentrenamiento` (`id`, `dificultad`, `titulo`, `descripcion`, `fecha_creacion`) VALUES
+(1, '', 'Plan de entrenamiento 0', 'Descripción del plan de entrenamiento 0', NULL),
+(2, '', 'Plan de entrenamiento 1', 'Descripción del plan de entrenamiento 1', NULL),
+(3, '', 'Plan de entrenamiento 2', 'Descripción del plan de entrenamiento 2', NULL),
+(4, '', 'Plan de entrenamiento 3', 'Descripción del plan de entrenamiento 3', NULL),
+(5, '', 'Plan de entrenamiento 4', 'Descripción del plan de entrenamiento 4', NULL),
+(6, '', 'Plan de entrenamiento 5', 'Descripción del plan de entrenamiento 5', NULL),
+(7, '', 'Plan de entrenamiento 6', 'Descripción del plan de entrenamiento 6', NULL),
+(8, '', 'Plan de entrenamiento 7', 'Descripción del plan de entrenamiento 7', NULL),
+(9, '', 'Plan de entrenamiento 8', 'Descripción del plan de entrenamiento 8', NULL),
+(10, '', 'Plan de entrenamiento 9', 'Descripción del plan de entrenamiento 9', NULL),
+(11, '', 'titulo de prueba', 'prueba de plan de entrenamiento', '2025-05-16');
 
 -- --------------------------------------------------------
 
@@ -218,7 +224,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `grupocontrata`
 --
 ALTER TABLE `grupocontrata`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `mensaje`
@@ -230,7 +236,7 @@ ALTER TABLE `mensaje`
 -- AUTO_INCREMENT de la tabla `planesentrenamiento`
 --
 ALTER TABLE `planesentrenamiento`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `tipousuario`
